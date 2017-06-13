b9f5310ac8b746e8d8cd03d455f21dbb23f3e1ba
Robert Geyer <Robert Geyer>
2017-06-13 19:25:00 -0400

    testing

---
+++ models/drones.js
@@ -0,0 +1,112 @@
+module.exports = function(sequelize, DataTypes) {
+  var drones = sequelize.define("drones", {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    drone_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1, 255]
+      }
+    },
+    price: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: "0"
+    },
+    camera: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false
+    },
+    camera_max: {
+      type: DataTypes.STRING,
+      validate: {
+        len: [1, 30]
+      }
+    },
+    video: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false
+    },
+    video_res: {
+      type: DataTypes.STRING,
+      validate: {
+        len: [1, 30]
+      }
+    },
+    video_fps: {
+      type: DataTypes.STRING,
+      validate: {
+        len: [1, 30]
+      }
+    },
+    max_range: {
+      type: DataTypes.STRING,
+      validate: {
+        len: [1, 30]
+      }
+    },
+    flight_time: {
+      type: DataTypes.STRING,
+      validate: {
+        len: [1, 30]
+      }
+    },
+    gimbal: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false
+    },
+    gps: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false
+    },
+    avoidance: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
+    },
+    headless: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
+    },
+    altitude: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
+    },
+    flips: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
+    },
+    max_speed: {
+      type: DataTypes.STRING,
+      validate: {
+        len: [1, 30]
+      }
+    },
+    weight: {
+      type: DataTypes.STRING,
+      validate: {
+        len: [1, 30]
+      }
+    },
+    intelligent_flight: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
+    },
+    picture_small: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    picture_large: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  });
+  return drones;
+};
---
+++ server.js
@@ -0,0 +1,58 @@
+var express = require("express"),
+  xAdmin = require('express-admin');
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
+// Requiring our models for syncing
+var db = require("./models");
+
+var PORT = process.env.PORT || 3000;
+
+var app = express();
+
+// Override with POST having ?_method=DELETE
+app.use(methodOverride("_method"));
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+var config = {
+  dpath: './express-admin-config/',
+  config: require('./express-admin-config/config.json'),
+  settings: require('./express-admin-config/settings.json'),
+  custom: require('./express-admin-config/custom.json'),
+  users: require('./express-admin-config/users.json')
+};
+
+xAdmin.init(config, function(err, admin) {
+  if (err) return console.log(err);
+  // mount express-admin before any other middlewares
+  app.use('/admin', admin);
+  // site specific middlewares
+  // Serve static content for the app from the "public" directory in the application directory.
+  app.use(express.static(process.cwd() + "/public"));
+  app.use(bodyParser.urlencoded({
+    extended: false
+  }));
+
+  // uncomment when handlebars are made ----
+  // app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+  // app.set("view engine", "handlebars");
+
+  // Import routes and give the server access to them.
+  var routes = require("./controllers/drone_controller.js");
+  // site routes
+  app.use("/", routes);
+  app.use("/single/:id", routes);
+  app.get('/', function(req, res) {
+    res.send('Hello World');
+  });
+
+  // site server
+  db.sequelize.sync({}).then(function() {
+    app.listen(PORT, function() {
+      console.log("App listening on PORT " + PORT);
+    });
+  });
+});
+
+//
